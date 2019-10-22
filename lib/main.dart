e5e6b48d30af0ae88cbd574a100e5eb2641f78a0
Tanzirul Haque Nayan <tanzirulhaque7@gmail.com>
2019-10-22 15:10:33 +0600

    StatelessWidget successful

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,15 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-// import 'package:english_words/english_words.dart';
+import 'package:english_words/english_words.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -17,7 +19,8 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          // child: Text('Hello World'),
+          child: Text(wordPair.asPascalCase)
         ),
       ),
     );
